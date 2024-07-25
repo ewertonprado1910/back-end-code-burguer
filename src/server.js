073a7a761c89eba1c60d99d126a0212414275e26
ewertonprado1910 <ewertonprado20@gmail.com>
2024-07-25 10:18:28 -0300

    atualização em server.js

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ const host = '0.0.0.0'
 const port = process.env.PORT || 3001;
 
 app.listen(port, `${host}`, () => {
-    console.log(`Server listening on [::]${port}`);
+    console.log(`Server listening on port ${port}`);
 });
 
