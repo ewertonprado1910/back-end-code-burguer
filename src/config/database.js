7071050f8a1ae524a5f5259212a3be3452a998c5
ewertonprado1910 <ewertonprado20@gmail.com>
2024-07-25 11:40:18 -0300

    mudança database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = { Sequelize, sequelizeErrors, 
     dialect: "postgres",
     url: "postgresql://postgres:ibHsXczKstXQDkNZnIDJIUxcaPoNrkdO@viaduct.proxy.rlwy.net:19794/railway",
     define: {
@@ -8,35 +8,3 @@ module.exports = {
     },
 }
 
-const { Sequelize, sequelizeErrors } = require('sequelize');
-
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-/*
-async function connectToDatabase() {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (err) {
-    if (err instanceof sequelizeErrors.ConnectionError) {
-      console.error('Unable to connect to the database:', err);
-     
-      return Promise.reject(new sequelizeErrors.ConnectionError(err));
-    } else {
-      
-      return Promise.reject(err);
-    }
-  }
-}
-
-connectToDatabase()
-  .then(() => {
-    console.log('Database connection successful');
-  })
-  .catch((err) => {
-    console.error('Database connection failed:', err);
-  });
-
-*/
