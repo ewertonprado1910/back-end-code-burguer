c94b4f6cf7e5bccf59b69c73f221871a6b0f9333
ewertonprado1910 <ewertonprado20@gmail.com>
2024-07-25 10:40:11 -0300

    atualizaçaõ

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
+    host: "localhost",
     dialect: "postgres",
     url: "postgresql://postgres:ibHsXczKstXQDkNZnIDJIUxcaPoNrkdO@viaduct.proxy.rlwy.net:19794/railway",
     define: {
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from "./app" 
 
 const host = '0.0.0.0'
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 
 app.listen(port, `${host}`, () => {
     console.log(`Server listening on port ${port}`);
