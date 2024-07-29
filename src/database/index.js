df679e20b7b7a556e3133aeab8070b56c4050d7a
ewertonprado1910 <ewertonprado20@gmail.com>
2024-07-29 11:46:00 -0300

    atualização em server, index e upload de imagens

--- src/database/index.js
+++ src/database/index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import User from "../app/models/User";
 import Product from "../app/models/Products"
 import Category from "../app/models/Caregory";
-import configDatabase from "../config/database"
+
 
 const models = [User, Product, Category]
 
@@ -15,7 +15,8 @@ class Database {
     }
 
     init() {
-        this.connection = new Sequelize(configDatabase)
+        this.connection = new Sequelize(
+            "postgresql://postgres:gksekyufOmGpqzFXNCvXyrKpTfVbuEJc@viaduct.proxy.rlwy.net:37056/railway")
         models
             .map(model => model.init(this.connection))
             .map(model => model.associate && model.associate(this.connection.models))
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import app from "./app" 
 
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server listening on port ${port}`);
 });
 
