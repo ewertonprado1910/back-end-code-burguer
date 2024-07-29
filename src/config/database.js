e5ffc613d2f37e640c38fc3b09e1b094bd5897b2
ewertonprado1910 <ewertonprado20@gmail.com>
2024-07-29 10:40:05 -0300

    alteração url postgres

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     dialect: "postgres",
-    url: "postgresql://postgres:ibHsXczKstXQDkNZnIDJIUxcaPoNrkdO@viaduct.proxy.rlwy.net:19794/railway",
+    url: "postgresql://postgres:gksekyufOmGpqzFXNCvXyrKpTfVbuEJc@viaduct.proxy.rlwy.net:37056/railway",
     define: {
         timestamps: "true",
         underscored: "true",
