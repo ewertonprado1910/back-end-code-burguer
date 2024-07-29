8a6c91517461d082f4d1b07a36d39b6ff0add703
ewertonprado1910 <ewertonprado20@gmail.com>
2024-07-29 10:11:11 -0300

    atualização database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,19 @@
 module.exports = {
+        sequelize: new Sequelize('database', 'username', 'password', {
+        host: 'localhost',
+        dialect: 'postgres',
+        port: 5432,
+        pool: {
+          max: 10,
+          min: 0,
+          acquire: 30000,
+          idle: 10000
+        },
+        dialectOptions: {
+          connectTimeout: 60000
+        }
+      }),
+      
     dialect: "postgres",
     url: "postgresql://postgres:ibHsXczKstXQDkNZnIDJIUxcaPoNrkdO@viaduct.proxy.rlwy.net:19794/railway",
     define: {
