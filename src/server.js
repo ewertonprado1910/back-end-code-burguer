2b9b0aa23ba2173b77cda7c9c4c0077e0f98585d
ewertonprado1910 <ewertonprado20@gmail.com>
2024-07-29 10:56:04 -0300

    mudança no server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import app from "./app" 
 
-const host = '0.0.0.0'
-const port = process.env.PORT || 3001;
 
-app.listen(port, `${host}`, () => {
+const port = 3001;
+
+app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
 
